632f75f8187684e7c6f14f4ce76bce02060c54aa
LikithJoelNandi <49515456+LikithJoelNandi@users.noreply.github.com>
2019-05-30 17:19:52 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,4 +4,4 @@ docker stop admin-ui
 docker rm admin-ui
 docker rmi admin-ui
 docker image build -t admin-ui ~/Admin-ui/.
-docker run -d -p 4300:4200 --name admin-ui admin-ui
+docker run -d -p 4300:80 --name admin-ui admin-ui
